ad8b4b2ced8ef3658b496c1ce3efe9d782612049
shinoyuki222 <tangxq93@126.com>
2020-05-04 22:31:48 +0800

    add beam search module

--- VAE_NLG/evaluate.py
+++ VAE_NLG/evaluate.py
@@ -4,7 +4,7 @@ import torch
 import os
 import model
 from dataloader import DataLoader
-from generator import generate_songci
+from generator import generator_beam
 
 parser = argparse.ArgumentParser(description='VAE-NLG')
 parser.add_argument('--data', type=str, default='./data/vae_nlg.pt',
@@ -27,6 +27,7 @@ args.idx2word = {v: k for k, v in data['word2idx'].items()}
 args.vocab_size = data['vocab_size']
 args.pre_w2v = data['pre_w2v']
 
+print('loading model...')
 
 save_dir = 'model'
 model_name = 'vae_nlg_full_model'
@@ -34,9 +35,10 @@ directory = os.path.join(save_dir,model_name+'.tar')
 ckpt = torch.load(directory)
 vae_model = model.VAE(args)
 vae_model.load_state_dict(ckpt['model'])
-poetry = generate_songci(vae_model,args, -1)
-print('loading model^')
-print("poetry generation - [{}]".format(poetry, encoding = 'utf-8',ascii=True))
+# poetry = generate_songci(vae_model,args, -1)
+scores, poetries = generator_beam(vae_model,args,2,5)
+for score,poetry in zip(scores,poetries):
+	print("poetry generation - [{0}] with score {1}".format(poetry, score,encoding = 'utf-8',ascii=True))
 print('-' * 90)
 # generate_songci(vae_model,args.idx2word)
 
