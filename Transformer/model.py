b45954844aeb23c3cbfcb3549ebc58db41ad476c
shinoyuki222 <tangxq93@126.com>
2020-05-10 18:09:03 +0800

    Update model.py

--- Transformer/model.py
+++ Transformer/model.py
@@ -13,8 +13,8 @@ def get_attn_pad_mask(seq_q, seq_k):
     batch_size, len_q = seq_q.size()
     batch_size, len_k = seq_k.size()
     # eq(zero) is PAD token
-    pad_attn_mask = seq_k.data.eq(0).unsqueeze(1)  # batch_size x 1 x len_k(=len_q), one is masking
-    return pad_attn_mask.expand(batch_size, len_q, len_k)  # batch_size x len_q x len_k
+    pad_attn_mask = seq_k.data.eq(0).unsqueeze(1)  # (batch_size, 1, len_k/len_q) one is masking
+    return pad_attn_mask.expand(batch_size, len_q, len_k)  # (batch_size, len_q, len_k)
 
 
 def positional_encoding(n_position, d_model):
@@ -51,23 +51,40 @@ class MultiHeadAttention(nn.Module):
 
     def forward(self, Q, K, V, attn_mask):
         # batch first
-        # q: (batch_size, len_q, d_model)
-        # k: (batch_size, len_k, d_model)
-        # v: (batch_size, len_k, d_model)
+        # Q: (batch_size, len_q, d_model)
+        # K: (batch_size, len_k, d_model)
+        # V: (batch_size, len_k, d_model)
+        # attn_mask: (batch_size, len_q, len_k)
         residual, batch_size = Q, Q.size(0)
         # (B, S, dk/d_q) -proj-> (B, S, D) -split-> (B, S, H, d_k(d_q)) -trans-> (B, H, S, d_k(d_q))
-        q_s = self.W_Q(Q).view(batch_size, -1, n_heads, d_k).transpose(1,2)  # q_s: [batch_size x n_heads x len_q x d_k]
-        k_s = self.W_K(K).view(batch_size, -1, n_heads, d_k).transpose(1,2)  # k_s: [batch_size x n_heads x len_k x d_k]
-        v_s = self.W_V(V).view(batch_size, -1, n_heads, d_v).transpose(1,2)  # v_s: [batch_size x n_heads x len_k x d_v]
+        q_s = self.W_Q(Q).view(batch_size, -1, n_heads, d_k).transpose(1,2)  # q_s: (batch_size, n_heads, len_q, d_k)
+        k_s = self.W_K(K).view(batch_size, -1, n_heads, d_k).transpose(1,2)  # k_s: (batch_size, n_heads, len_k, d_k)
+        v_s = self.W_V(V).view(batch_size, -1, n_heads, d_v).transpose(1,2)  # v_s: (batch_size, n_heads, len_k, d_v)
 
-        attn_mask = attn_mask.unsqueeze(1).repeat(1, n_heads, 1, 1) # attn_mask : [batch_size x n_heads x len_q x len_k]
+        #(B, len_q, len_k) -unsqueeze(1)-> (B, 1, len_q, len_k) - repeat -> (B, n_heads, len_q, len_k)
+        attn_mask = attn_mask.unsqueeze(1).repeat(1, n_heads, 1, 1) # attn_mask : (batch_size, n_heads, len_q, len_k)
 
-        # context: [batch_size x n_heads x len_q x d_v], attn: [batch_size x n_heads x len_q(=len_k) x len_k(=len_q)]
+        # context: (batch_size, n_heads, len_q, d_v)
+        # attn: (batch_size, n_heads, len_q/len_k, len_k/len_q)
         context, attn = ScaledDotProductAttention()(q_s, k_s, v_s, attn_mask)
-        context = context.transpose(1, 2).contiguous().view(batch_size, -1, n_heads * d_v) # context: [batch_size x len_q x n_heads * d_v]
+        # context: (batch_size, n_heads, len_q, d_v) -trans->(batch_size, len_q, n_heads, d_v) -reshape->(batch_size , len_q , n_heads * d_v)
+        context = context.transpose(1, 2).reshape(batch_size, -1, n_heads * d_v) # context: (batch_size , len_q , n_heads * d_v)
+        # output: (batch_size, len_q, d_model)
         output = nn.Linear(n_heads * d_v, d_model)(context)
-        return nn.LayerNorm(d_model)(output + residual), attn # output: [batch_size x len_q x d_model]
+        return nn.LayerNorm(d_model)(output + residual), attn 
 
+class PoswiseFFN(nn.Module):
+    def __init__(self):
+        super(PoswiseFFN, self).__init__()
+        self.conv1 = nn.Conv1d(in_channels=d_model, out_channels=d_ff, kernel_size=1)
+        self.conv2 = nn.Conv1d(in_channels=d_ff, out_channels=d_model, kernel_size=1)
+
+    def forward(self, inputs):
+        # inputs : (batch_size, len_q, d_model)
+        residual = inputs 
+        output = nn.ReLU()(self.conv1(inputs.transpose(1, 2)))
+        output = self.conv2(output).transpose(1, 2)
+        return nn.LayerNorm(d_model)(output + residual)
 
 
 sentences = ['我 喜欢 机器 学习', 'S i like machine learning', 'i like machine learning E']
@@ -88,7 +105,7 @@ d_model = 512  # Embedding Size
 d_ff = 2048 # FeedForward dimension
 d_k = d_v = 64  # dimension of K(=Q), V
 n_layers = 6  # number of Encoder of Decoder Layer
-n_heads = 8  # number of heads in Multi-Head Attention
+n_heads = 4  # number of heads in Multi-Head Attention
 
 def make_batch(sentences):
     input_batch = [[src_vocab[n] for n in sentences[0].split()]]
@@ -105,6 +122,12 @@ enc_self_attn_mask = get_attn_pad_mask(enc_inputs, enc_inputs)
 
 context, attn = ScaledDotProductAttention()(enc_outputs, enc_outputs, enc_outputs, enc_self_attn_mask)
 
-# print(enc_outputs.size())
+ans, attn = MultiHeadAttention()(enc_outputs, enc_outputs, enc_outputs, enc_self_attn_mask)
+
+print(context.size())
+print(context.transpose(1,2).is_contiguous())
+# print(context.transpose(1,2).reshape(1, -1, n_heads * d_v))
+
+print(enc_outputs.size())
 # print(enc_outputs.view(-1, 8, 256).size())
 
